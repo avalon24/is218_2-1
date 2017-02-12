e1a8915f3ad97dad3773966476bbccb878b77e5c
ankan basu <ab936@afsconnect1.njit.edu>
2017-02-12 07:03:15 -0500

    Added number formatting and modified the respective echos

--- display_discount.php
+++ display_discount.php
@@ -6,6 +6,11 @@
    $discount_amt = $list_price * $discount_pct * 0.01;
    $discount_price = $list_price - $discount_amt;
 
+   $list_price_f     = "$".number_format($list_price,2);
+   $discount_pct_f   = number_format($discount_pct_f,1)."%";
+   $discount_amt_f   = "$".number_format($discount_amt,2);
+   $discount_price_f = "$".number_format($discount_price,2);  
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,16 +26,16 @@
         <span><?php echo $product_desc; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price; ?></span><br>
+        <span><?php echo $list_price_f; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_pct; ?></span><br>
+        <span><?php echo $discount_pct_f; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo $discount_amt; ?></span><br>
+        <span><?php echo $discount_amt_f; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo $discount_price; ?></span><br>
+        <span><?php echo $discount_price_f; ?></span><br>
     </main>
 </body>
 </html>
