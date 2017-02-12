41360a6a34d1a31397d0ad4aa03fc65a359e5970
ankan basu <ab936@afsconnect1.njit.edu>
2017-02-12 06:43:04 -0500

    Modified first 3 echo statements with the new values received

--- display_discount.php
+++ display_discount.php
@@ -16,13 +16,13 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_desc; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_pct; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
